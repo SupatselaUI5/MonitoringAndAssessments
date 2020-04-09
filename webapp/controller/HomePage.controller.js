6753f3a2afb53a314830c5f49c602ee7c8399a67
Neo Lehoko <lehokon@gmail.com>
2020-04-09 09:17:44 +0000

    Binding

--- webapp/controller/HomePage.controller.js
+++ webapp/controller/HomePage.controller.js
@@ -2,7 +2,7 @@ sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
 	"use strict";
 	return Controller.extend("gdsd.Monitoring.controller.HomePage", {
 		onInit: function () {
-			this.oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			// this.oRouter = sap.ui.core.UIComponent.getRouterFor(this);
 			// this.oRouter.getRoute("RouteHomePage").attachPatternMatched(this._onObjectMatched, this);
 			this.onBindSWBP();
 			var today = new Date();
@@ -11,30 +11,51 @@ sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
 		},
 
 		_onObjectMatched: function () {
-
+			// alert( "")
 		},
 
 		onBindSWBP: function () {
-				// sap.ui.core.BusyIndicator.show();
-				this._oODataModel = this.getOwnerComponent().getModel();
-				this._oODataModel.setDefaultBindingMode(sap.ui.model.BindingMode.TwoWay);
-				this._oODataModel.read("/GET_BPSet('1000000022')", {
-					//User details retrieved successfully
-					success: (function (oData) {
-						// console.log("Success " + oData);
-						this.byId("objHeaderSW").setModel(this._oODataModel);
-						this.byId("objHeaderSW").bindElement({
-							path: "/GET_BPSet('1000000022')"
-								// use OData parameters here if needed
-						});
-						sap.ui.core.BusyIndicator.hide();
-						// this.getView().setModel(this._oODataModel);
-						// this.byId("tblOfficial").setModel(this._oODataModel);
-						// this.byId("inpOffName").bindElement({path : "/GET_BPSet('245')"});
-					}).bind(this),
-					error: (function (e, x, r) {
-						// console.log("Error " + e);
-					})
+			sap.ui.core.BusyIndicator.show();
+
+			this._oODataModel = this.getOwnerComponent().getModel();
+			this._oODataModel.setDefaultBindingMode(sap.ui.model.BindingMode.TwoWay);
+			this._oODataModel.read("/GET_BPSet", {
+				//User details retrieved successfully
+				success: (function (oData) {
+					var BPJsonModel = new sap.ui.model.json.JSONModel({
+						data: oData.results[0]
+					});
+					this.byId("objHeaderSW").setModel(BPJsonModel);
+					this.byId("objHeaderSW").bindElement({
+						path: "/data"
+							// use OData parameters here if needed
+					});
+					var BPNo = oData.results[0].But000.Partner;
+					var filterVal = "BpNo eq '" + BPNo + "'";
+					this.getTaskData(filterVal);
+					sap.ui.core.BusyIndicator.hide();
+				}).bind(this),
+				error: (function (e, x, r) {
+					// console.log("Error " + e);
+				})
+			});
+		},
+
+		getTaskData: function (filter) {
+				this._oODataModel.read("/Get_Funding_TaskSet", { // sPath - path of your Entityset
+					urlParameters: {
+						"$filter": filter
+					},
+					success: function (data, response) {
+						var TaskJsonModel = new sap.ui.model.json.JSONModel({
+							data: data.results[0]
+						}, "Task");
+						this.byId("taskList").setModel(TaskJsonModel);
+						//your code for manipulation of the data received 
+					}.bind(this), // if you want to use the current controller instance within this function
+					error: function (response) {
+							// for handling the error received
+						}.bind(this) // if you want to use the current controller instance within this function
 				});
 			},
 			/**
